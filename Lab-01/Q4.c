55658cc1c43928b296e78ac05e19da25b1ac3295
Prabeen Kumar Swain <b125082@iiit-bh.ac.in>
2026-07-30 12:49:39 +0530

    Add Towers of Hanoi implementation
    Implement Towers of Hanoi algorithm in C.

---
+++ Lab-01/Q4.c
@@ -0,0 +1,35 @@
+//Towers of Hanoi (ToH)
+#include <stdio.h>
+
+long long moves = 0;
+
+void toh(int n,char from,char aux,char to)
+{
+    if(n==1)
+    {
+        printf("Move disk 1 from %c to %c\n",from,to);
+        moves++;
+        return;
+    }
+
+    toh(n-1,from,to,aux);
+
+    printf("Move disk %d from %c to %c\n",n,from,to);
+    moves++;
+
+    toh(n-1,aux,from,to);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of disks : ");
+    scanf("%d",&n);
+
+    toh(n,'A','B','C');
+
+    printf("\nTotal Moves = %lld\n",moves);
+
+    return 0;
+}
